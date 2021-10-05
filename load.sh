50c87296b5e3f4a77d732f7bf119e42d790108cd
Tony <tony@stark.com>
2021-10-05 09:58:11 +0000

    multiverse

--- load.sh
+++ load.sh
@@ -1,25 +1,42 @@
 #!/bin/bash
+# Ubuntu 20.04.3 LTS
+
 set -ex
-SHA1=$(sudo fdisk -l /dev/sdb | grep /dev/ | sha256sum)
-SHA2=$(sudo sha256sum /dev/sdb2)
-SHA3=$(sudo sha256sum /dev/sdb1)
 
-SHA1a="7fb1f3cca9d2c1ecbd7c50ea34ab389433fe740101866165ac74918c6f01f511  -"
-SHA2a="8fffdc9fec84d1796caf570f43151e65f8131d174bbb474d3990012c6f426668  /dev/sdb2"
-SHA3a="93bdab204067321ff131f560879db46bee3b994bf24836bb78538640f689e58f  /dev/sdb1"
+containsElement () {
+  local e match="$1"
+  shift
+  for e; do [[ "$e" == "$match" ]] && return 0; done
+  return 1
+}
 
+FDISK_ARRAY=("7fb1f3cca9d2c1ecbd7c50ea34ab389433fe740101866165ac74918c6f01f511" "8a4572d8dbab496886494dff0124debebe54866d45d99f29009ce9d29ff113d0")
+SDB2_ARRAY=("8fffdc9fec84d1796caf570f43151e65f8131d174bbb474d3990012c6f426668" "8fffdc9fec84d1796caf570f43151e65f8131d174bbb474d3990012c6f426668")
+SDB1_ARRAY=("93bdab204067321ff131f560879db46bee3b994bf24836bb78538640f689e58f" "5fdebc435ded46ae99136ca875afc6f05bde217be7dd018e1841924f71db46b5")
 
-if [[ "$SHA1" != "$SHA1a" ]] || [[ "$SHA2" != "$SHA2a" ]] || [[ "$SHA3" != "$SHA3a" ]]; then
-echo "hashes are not equal"
-echo $SHA1
-echo $SHA1a
-echo $SHA2
-echo $SHA2a
-echo $SHA3
-echo $SHA3a
-exit 1
-fi
-echo "hashes are ok"
+SHA_FDISK=$(sudo fdisk -l /dev/sdb | grep /dev/ | sha256sum |awk '{print $1;}' )
+if ! containsElement "$SHA_FDISK" "${FDISK_ARRAY[@]}"; then 
+	echo "fdisk $SHA_FDISK is not in $FDISK_ARRAY";
+	exit 1 
+fi;
+echo "fdisk ok"
+
+SHA_SDB2=$(sudo sha256sum /dev/sdb2| awk '{print $1;}')
+if ! containsElement "$SHA_SDB2" "${SDB2_ARRAY[@]}"; then 
+        echo "sdb2 $SHA_SDB2 is not in $SDB2_ARRAY";
+        exit 1 
+fi;
+echo "sdb2 ok"
+
+SHA_SDB1=$(sudo sha256sum /dev/sdb1| awk '{print $1;}')
+if ! containsElement "$SHA_SDB1" "${SDB1_ARRAY[@]}"; then 
+        echo "sdb1 $SHA_SDB1 is not in $SDB1_ARRAY";
+        exit 1 
+fi;
+echo "sdb1 ok"
+
+echo "all ok"
+
 
 while lsblk | grep "sdb" > /dev/null;
 do
