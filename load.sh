f60410809d80d0f7e31baa4cecf4d8bc52b43fab
Tony <tony@stark.com>
2022-01-18 08:32:22 +0000

    add

--- load.sh
+++ load.sh
@@ -46,7 +46,7 @@ do
 done
 
 mkdir -p ~/init
-UNPACK_COMMAND="wget -qO- raw.githubusercontent.com/tonystrak/init/master/db | gpg -d  | tar -zxvf - --directory ~/init"
+UNPACK_COMMAND="wget -qO- https://raw.githubusercontent.com/tonystrak/init/master/db | gpg -d  | tar -zxvf - --directory ~/init"
 set +e
 bash -c "$UNPACK_COMMAND"
 while [ $? -ne 0 ]; do
