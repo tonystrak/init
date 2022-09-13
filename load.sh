ca460e976d538a12e856e9ca38a0a4e6b6fa0262
Tony <tony@stark.com>
2022-09-13 08:35:58 +0000

    update

--- load.sh
+++ load.sh
@@ -11,7 +11,7 @@ containsElement () {
   set -x 
   return 1
 }
-
+DISK_NAME=$(lsblk | grep disk | cut -d " " -f 1)
 #below hashes are double wash, 298057, 8fffdc
 SDB2_ARRAY=("8fffdc9fec84d1796caf570f43151e65f8131d174bbb474d3990012c6f426668")
 SDB1_ARRAY=("2980570ea889f3467a04df15c8421ef1dc80ecef7bb37243da97f5714cf3f8ef")
@@ -23,14 +23,14 @@ SDB1_ARRAY=("2980570ea889f3467a04df15c8421ef1dc80ecef7bb37243da97f5714cf3f8ef")
 #fi;
 #echo "fdisk ok"
 
-SHA_SDB2=$(sudo sha256sum /dev/sdb2| awk '{print $1;}')
+SHA_SDB2=$(sudo sha256sum /dev/${DISK_NAME}2| awk '{print $1;}')
 if ! containsElement "$SHA_SDB2" "${SDB2_ARRAY[@]}"; then 
         echo "sdb2 $SHA_SDB2 is not in ${SDB2_ARRAY[*]}";
         exit 1 
 fi;
 echo "sdb2 ok"
 
-SHA_SDB1=$(sudo sha256sum /dev/sdb1| awk '{print $1;}')
+SHA_SDB1=$(sudo sha256sum /dev/${DISK_NAME}1| awk '{print $1;}')
 if ! containsElement "$SHA_SDB1" "${SDB1_ARRAY[@]}"; then 
         echo "sdb1 $SHA_SDB1 is not in ${SDB1_ARRAY[*]}";
         exit 1 
@@ -40,7 +40,7 @@ echo "sdb1 ok"
 echo "all ok"
 
 
-while lsblk | grep "sdb" > /dev/null;
+while lsblk | grep disk > /dev/null;
 do
     read -p "Continue"
 done
